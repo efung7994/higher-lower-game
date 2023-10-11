0b1b1fed4ca104fa1ec9b5afc7c57a4e47f5e4a1
Eugene <eugenefung79@gmail.com>
2023-10-11 13:56:25 -0400

    create function to choose randomly form game data

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import art
 import game_data
+import random
+
+
+def game():
+  def choices():
+    choice = random.choice(game_data.data)
+    game_data.data.remove(choice)
+    name = choice["name"]
+    description = choice["description"]
+    country = choice["country"]
+    folowers = choice["follower_count"]
+    print(f"{name}, a {description} from {country}")
+
+  
+  print(art.logo)
+  print("Let's play higher or lower!")
+  print("Who has more followers?")
+  choice_1 = choices()
+
+  print(art.vs)
+  choice_2 = choices()
+  print(choice_2["followers"])
+  print("higher or lower?")
+
+game()
