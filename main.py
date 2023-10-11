34547977affdb8a6ce4037c4b30d57cc713b2e29
Eugene <eugenefung79@gmail.com>
2023-10-11 16:18:58 -0400

    game functionality complete. need to fix bug where choice 1 needs to be the winner of the higher lower comparison

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
 import art
 import game_data
 import random
 
 followers_global = 0
 choice_global = None
+points = 0
+choice_message = ''
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 
 def game():
-  
+  cls()
   def choices():
     choice = random.choice(game_data.data)
     global choice_global
@@ -18,39 +25,60 @@ def game():
     followers = int(choice["follower_count"])
     global followers_global
     followers_global = followers
-    print(f"{name}, a {description} from {country}")
+    global choice_message
+    choice_message = f"{name}, a {description} from {country} \n"
 
   choice_1 = choices()
 
   def compare():
+    global points
     print(art.logo)
+    print(f"You have {points} points")
     print("Let's play higher or lower!")
-    print("Who has more followers?")
+    print("Who has more followers? \n")
 
     nonlocal choice_1
     choice_1 = choice_1
+    print(choice_message)
     followers_1 = followers_global
-    print(followers_global)
-    print(choice_global)
     
-
     print(art.vs)
     
     choice_2 = choices()
+    print(choice_message)
     followers_2 = followers_global
-    print(followers_global)
-    print(choice_global)
 
     answer = input("A or B? \n")
     
-    if answer.lower() == "a" or "b":
+    if answer.lower() == "a":
       if followers_1 > followers_2:
         choice_1 = choice_global
-        print(choice_global)
-        print(choice_1)
+        points += 1
+        cls()
         compare()
       else:
-        print("Wrong")
+        cls()
+        print("Wrong. GAME OVER")
+        print(f"You scored {points} points!")
+        replay = input("Play again? y/n \n")
+        if replay.lower() == "y":
+          game()
+    
+    if answer.lower() == "b":
+      if followers_1 < followers_2:
+        choice_1 = choice_global
+        points += 1
+        print(f"You have {points} points")
+        cls()
+        compare()
+      else:
+        cls()
+        print("Wrong. GAME OVER")
+        print(f"You scored {points} points!")
+        replay = input("Play again? y/n \n")
+        if replay.lower() == "y":
+          game()
+
 
   compare()
 
