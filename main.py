8331c58baba40a3861c75d44eca75326e4e07140
Eugene <eugenefung79@gmail.com>
2023-10-11 15:28:30 -0400

    create comparison funciton. bug testing

--- main.py
+++ main.py
@@ -2,26 +2,51 @@ import art
 import game_data
 import random
 
+followers_global = 0
+choice_global = None
 
 def game():
+  
   def choices():
     choice = random.choice(game_data.data)
+    global choice_global
+    choice_global = choice
+    print(choice_global)
     game_data.data.remove(choice)
     name = choice["name"]
     description = choice["description"]
     country = choice["country"]
-    folowers = choice["follower_count"]
+    followers = int(choice["follower_count"])
+    global followers_global
+    followers_global = followers
     print(f"{name}, a {description} from {country}")
 
-  
-  print(art.logo)
-  print("Let's play higher or lower!")
-  print("Who has more followers?")
-  choice_1 = choices()
+  def compare():
+    print(art.logo)
+    print("Let's play higher or lower!")
+    print("Who has more followers?")
 
-  print(art.vs)
-  choice_2 = choices()
-  print(choice_2["followers"])
-  print("higher or lower?")
+    choice_1 = choices()
+    followers_1 = followers_global
+    print(followers_global)
+    
+
+    print(art.vs)
+    
+    choice_2 = choices()
+    followers_2 = followers_global
+    print(followers_global)
+
+    answer = input("A or B? \n")
+    
+    if answer.lower() == "a":
+      if followers_1 > followers_2:
+        choice_1 = choice_global
+        choice_2 = choices()
+        compare()
+      else:
+        print("Wrong")
+
+    compare()
 
 game()
